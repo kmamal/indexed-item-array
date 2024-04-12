1acca9651c5249e52868e6af78958cb4ffdc3ce6
KM <the.real.laplace@gmail.com>
2024-04-13 00:17:15 +0100

    wip

---
+++ src/index.js
@@ -0,0 +1,32 @@
+
+class IndexedItemArray {
+	constructor (array, indexKey) {
+		this._array = array
+		this._indexKey = indexKey
+	}
+
+	at (i) { return this._array.at(i) }
+
+	set (i, item) {
+		this._array[i] = item
+		item[this._indexKey] = i
+	}
+
+	push (item) {
+		item[this._indexKey] = this._array.length
+		this._array.push(item)
+	}
+
+	removeItem (item) {
+		this.removeIndex(item[this._indexKey])
+	}
+
+	removeIndex (index) {
+		if (index !== this._array.length - 1) {
+			this.set(index, this._array.at(-1))
+		}
+		this._array.length--
+	}
+}
+
+module.exports = { IndexedItemArray }
